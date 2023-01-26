6c133a8082ea422663c4b76b2136a50f6525226f
kateewhy <78312222+kateewhy@users.noreply.github.com>
2023-01-26 18:29:04 -0500

    Added Main Menu Screen

---
+++ Assets/Scripts/MainMenuUI.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour
+{
+    public string Apartment;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(Apartment);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+
+        Debug.Log("Quit");
+    }
+}
