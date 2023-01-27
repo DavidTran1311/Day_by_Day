cbd30227256e09fc42ea48e86a57d520e18a4fa9
kateewhy <78312222+kateewhy@users.noreply.github.com>
2023-01-26 22:15:45 -0500

    Fixed UI Bugs

--- Assets/Scripts/MainMenuUI.cs
+++ Assets/Scripts/MainMenuUI.cs
@@ -2,26 +2,67 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuUI : MonoBehaviour
 {
     public string Apartment;
 
+    public Image fadeScreen;
+    public GameObject fadePanel;
+    public float fadeSpeed;
+    public bool shouldFadeToBlack, shouldFadeFromBlack;
+
+    public static MainMenuUI instance;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        fadePanel.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (shouldFadeToBlack)
+        {
+            fadeScreen.color = new Color(fadeScreen.color.r, fadeScreen.color.g, fadeScreen.color.b, Mathf.MoveTowards(fadeScreen.color.a, 1f, fadeSpeed * Time.deltaTime));
+            if(fadeScreen.color.a == 1f)
+            {
+                shouldFadeToBlack = false;
+            }
+        }
+
+        if (shouldFadeFromBlack)
+        {
+            fadeScreen.color = new Color(fadeScreen.color.r, fadeScreen.color.g, fadeScreen.color.b, Mathf.MoveTowards(fadeScreen.color.a, 0f, fadeSpeed * Time.deltaTime));
+            if (fadeScreen.color.a == 0f)
+            {
+                shouldFadeFromBlack = false;
+            }
+        }
+    }
+
+    public void FadeToBlack()
+    {
+        fadePanel.SetActive(true);
+        shouldFadeToBlack = true;
+        shouldFadeFromBlack = false;
+    }
+
+    public void FadeFromBlack()
+    {
+        shouldFadeFromBlack = true;
+        shouldFadeToBlack = false;
     }
 
     public void StartGame()
     {
         SceneManager.LoadScene(Apartment);
+
+        FadeToBlack();
+        fadePanel.SetActive(false);
     }
 
     public void QuitGame()
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,7 +15,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       
+        MainMenuUI.instance.FadeFromBlack();
     }
 
     // Update is called once per frame
