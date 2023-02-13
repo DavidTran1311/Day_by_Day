3f7d3a584fe650b91a7e62fcfa758e55bf276de7
kateewhy <78312222+kateewhy@users.noreply.github.com>
2023-02-12 21:02:30 -0500

    UI fixes and fade screen

---
+++ Assets/Scripts/OtherUI.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class OtherUI : MonoBehaviour
+{
+    public string Apartment;
+    public string Menu;
+
+    public GameObject gameOverPanel;
+    public GameObject summaryBG;
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameOverPanel.SetActive(false);
+        summaryBG.SetActive(true);
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameOverScreen();
+    }
+   
+    public void FadeToLevel()
+    {
+        animator.SetTrigger("FadeToBlack");
+    }
+
+
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(Menu);
+    }
+
+    public void GameOverScreen()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            summaryBG.SetActive(false);
+            gameOverPanel.SetActive(true);
+
+            //Debug.Log("press");
+        }
+    }
+}
