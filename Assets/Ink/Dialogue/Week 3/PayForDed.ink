30b48fc9b60fe6cb8409f781eafc91663ad75d71
kateewhy <78312222+kateewhy@users.noreply.github.com>
2023-04-09 22:45:21 -0400

    yo george is rlly ded

---
+++ Assets/Ink/Dialogue/Week 3/PayForDed.ink
@@ -0,0 +1,16 @@
+-> main
+
+=== main ===
+... #speaker: Dr. Yang #portrait: KT_neutral #speaker_color: F03D62
+I'm sorry to interrupt... #speaker: Dr. Yang #portrait: KT_neutral #speaker_color: F03D62
+We need to discuss some financial matters. #speaker: Dr. Yang #portrait: KT_neutral #speaker_color: F03D62
++ Get out. #speaker:Mr. Davis #portrait: davis_neutral #speaker_color: 3D77FF
+-> hospitalBills
++ My son is dead! #speaker:Mr. Davis #portrait: davis_neutral #speaker_color: 3D77FF
+-> hospitalBills
+
+=== hospitalBills ===
+I'm sorry... #speaker: Dr. Yang #portrait: KT_neutral #speaker_color: F03D62
+...the bill will be sent to you. #speaker: Dr. Yang #portrait: KT_neutral #speaker_color: F03D62
+
+-> END
---
+++ Assets/Scripts/Dialogue/DialogueSpace.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DialogueSpace : MonoBehaviour
+{
+    [Header("Ink JSON")]
+    [SerializeField] private TextAsset inkJSON;
+
+    public GameObject popUpPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        popUpPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !Dialogue_Manager.GetInstance().DialogueIsPlaying)
+        {
+            Dialogue_Manager.GetInstance().EnterDialogueMode(inkJSON);
+            
+
+        }
+        else if (Input.GetKeyDown(KeyCode.E) && !Dialogue_Manager.GetInstance().DialogueIsPlaying)
+        {
+            EndWeek();
+        }
+    }
+
+    public void EndWeek()
+    {
+        if (!Dialogue_Manager.GetInstance().DialogueIsPlaying)
+        {
+            popUpPanel.SetActive(true);
+        }
+    }
+
+    public void EndWeekScreen()
+    {
+        SceneManager.LoadScene("EndScreenWeek3");
+    }
+}
